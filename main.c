3ba4c5e76167c13c9225c88d7973d45d5f49ff03
Posu28 <89395763+Posu28@users.noreply.github.com>
2021-10-07 17:08:34 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main() {
+    int x,num=22;
+    x=num<<1;
+    printf("%d \n",x);
+    x=num<<2;
+    printf("%d \n",x);
+    x=num>>1;
+    printf("%d \n",x);
+    x=num>>2;
+    printf("%d \n",x);
+    return 0;
+}
